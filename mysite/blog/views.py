db2616775d8f1dea4a4f945dd890329dfa7cdb62
183-5 <2297009607@qq.com>
2019-06-25 19:50:06 +0800

    第一章7 为视图创建模板

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,6 +8,5 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status="published", publish__year=year, publish__month=month,
-                             publish__day=day)
+    post = get_object_or_404(Post, slug=post, status="published", publish__year=year, publish__month=month,publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
