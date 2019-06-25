9371131c1ca8c1e95886548284ce9708efb8f279
183-5 <2297009607@qq.com>
2019-06-25 20:05:54 +0800

    第一章8 添加分页功能

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,9 +3,21 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from .models import Post
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 每页显示3篇文章
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果page参数不是一个整数就返回第一页
+        posts = paginator.page(1)
+    except EmptyPage:
+        # 如果页数超出总页数就返回最后一页
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status="published", publish__year=year, publish__month=month,publish__day=day)
